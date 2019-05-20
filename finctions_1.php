7e1cb716a131b91356b8b1172468204ed8c8be32
Aaron Smusz <skaboy71@gmail.com>
2019-05-19 21:50:25 -0700

    Create finctions_1.php

---
+++ finctions_1.php
@@ -0,0 +1,77 @@
+<?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+date_default_timezone_set('America/Dawson_Creek');
+
+function prettyPrint( $json )
+{
+    $result = '';
+    $level = 0;
+    $in_quotes = false;
+    $in_escape = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if ( $in_escape ) {
+            $in_escape = false;
+        } else if( $char === '"' ) {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                case '{': case '[':
+                    $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+            }
+        } else if ( $char === '\\' ) {
+            $in_escape = true;
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= "\n".str_repeat( "\t", $new_line_level );
+        }
+        $result .= $char.$post;
+    }
+
+    return $result;
+}
+
+function csvToArray($file, $delimiter) { 
+          if (($handle = fopen($file, 'r')) !== FALSE) { 
+            $i = 0; 
+            while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
+              for ($j = 0; $j < count($lineArray); $j++) { 
+                $arr[$i][$j] = $lineArray[$j]; 
+              } 
+              $i++; 
+            } 
+            fclose($handle); 
+          } 
+          return $arr; 
+        } 
+?>
